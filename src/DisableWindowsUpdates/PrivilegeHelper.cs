5a26ca83a26bf43baeb14a15a8ae222a2025c5d8
Miquel TA <134692660+Miquel-TA@users.noreply.github.com>
2025-11-04 12:55:17 +0100

    Make project compatible with .NET Framework 4.8.1 and add logging

--- src/DisableWindowsUpdates/PrivilegeHelper.cs
+++ src/DisableWindowsUpdates/PrivilegeHelper.cs
@@ -2,45 +2,58 @@ using System;
 using System.Diagnostics;
 using System.Security.Principal;
 
-namespace DisableWindowsUpdates;
-
-internal static class PrivilegeHelper
+namespace DisableWindowsUpdates
 {
-    public static bool IsRunningAsAdministrator()
+    internal static class PrivilegeHelper
     {
-        using var identity = WindowsIdentity.GetCurrent();
-        var principal = new WindowsPrincipal(identity);
-        return principal.IsInRole(WindowsBuiltInRole.Administrator);
-    }
-
-    public static bool TryRelaunchWithElevation()
-    {
-        try
+        public static bool IsRunningAsAdministrator()
         {
-            using var currentProcess = Process.GetCurrentProcess();
-            var executablePath = currentProcess.MainModule?.FileName;
-            if (string.IsNullOrEmpty(executablePath))
-            {
-                executablePath = AppDomain.CurrentDomain.SetupInformation.ApplicationName;
-            }
-
-            if (string.IsNullOrEmpty(executablePath))
+            var identity = WindowsIdentity.GetCurrent();
+            if (identity == null)
             {
                 return false;
             }
 
-            var startInfo = new ProcessStartInfo(executablePath)
+            using (identity)
             {
-                UseShellExecute = true,
-                Verb = "runas"
-            };
-
-            Process.Start(startInfo);
-            return true;
+                var principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
         }
-        catch
+
+        public static bool TryRelaunchWithElevation()
         {
-            return false;
+            try
+            {
+                using (var currentProcess = Process.GetCurrentProcess())
+                {
+                    var executablePath = currentProcess.MainModule != null ? currentProcess.MainModule.FileName : null;
+                    if (string.IsNullOrEmpty(executablePath))
+                    {
+                        executablePath = AppDomain.CurrentDomain.SetupInformation.ApplicationName;
+                    }
+
+                    if (string.IsNullOrEmpty(executablePath))
+                    {
+                        return false;
+                    }
+
+                    var startInfo = new ProcessStartInfo(executablePath)
+                    {
+                        UseShellExecute = true,
+                        Verb = "runas"
+                    };
+
+                    Process.Start(startInfo);
+                    Logger.Info("Application relaunched with elevation request.");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Failed to relaunch application with elevation.", ex);
+                return false;
+            }
         }
     }
 }
